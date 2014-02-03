2e92a942ad07975cb8cd50b5b7730641d7ce5ce1
Félix Miguel Sanjuán Segovia <fmsanse@gmail.com>
2014-02-03 17:33:57 +0100

    Creación de excepción para Roisin
    - Se crea clase RoisinException.

---
+++ src/main/java/exception/RoisinException.java
@@ -0,0 +1,13 @@
+package exception;
+
+@SuppressWarnings("serial")
+public class RoisinException extends Exception {
+
+	public RoisinException() {
+		super();
+	}
+
+	public RoisinException(String message) {
+		super(message);
+	}
+}
